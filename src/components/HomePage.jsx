32833a3bde6575cdf9007b58e27504aaa605e182
colesouthern <csouthern333@gmail.com>
2023-06-10 16:21:02 -0400

    Improved look of disclaimer.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,6 +3,8 @@ import Carousel from '@/src/components/Carousel'
 import Image from 'next/image'
 import { useStore } from '@/src/hooks/useStore'
 
+import styles from '@/styles/Home.module.css'
+
 const HomePage = () => {
     const showSearchResults = useStore((state) => state.showSearchResults);
     const searchValue = useStore((state) => state.searchValue);
@@ -57,22 +59,19 @@ const HomePage = () => {
                         tmdbQuery={'trending/movie/week'}
                     />
                     <br />
-                    <footer>
-                        <br />
-                        <div>
-                            This product uses
-                        </div>
-                        <Image
-                            src="/blue_long_TMDB.svg"
-                            alt="TMDB Logo"
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                        <div>
-                            API but is not endorsed or certified by TMDB.
-                        </div>
-                        <br />
+                    <footer className={styles.appFooter}>
+                        <p className={styles.tmdbDisclaimer}>
+                            This product uses 
+                            <Image
+                                src="/blue_long_TMDB.svg"
+                                alt="TMDB Logo"
+                                width={100}
+                                height={24}
+                                priority
+                                style={{paddingLeft: '4px', paddingRight: '4px', marginBottom: '-8px'}}
+                            />
+                             API but is not endorsed or certified by TMDB.
+                        </p>
                     </footer>
                 </>
         }</>
